dccb12e5a4865c505adefffbaf54c13c8c1966d5
Marcos Fernández Pérez <mfernandez@edpuzzle.com>
2024-04-08 11:50:51 +0200

    Added Github Pages

--- src/routes/turns/+page.svelte
+++ src/routes/turns/+page.svelte
@@ -1,14 +1,80 @@
 <script lang="ts">
-	import { Card, Button, Label, Input } from 'flowbite-svelte';
+	import { Card, Button, Label, Input, Spinner } from 'flowbite-svelte';
+    import { toastMessageSuccess, toastSuccess, toastMessageAlert, toastAlert, toastMessageWarning, toastWarning } from '$lib/store';
     import { db } from '$lib/db';
 	import { liveQuery } from 'dexie';
 
     var date = new Date();
     var firstDay = new Date(date.getFullYear(), date.getMonth(), 2).toISOString().split('T')[0];
     var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 1).toISOString().split('T')[0];
-
     let turns = liveQuery(() => db.turn.where('date').between(firstDay, lastDay).toArray());
+    let schedules = liveQuery(() => db.schedule.toArray());
 
+    let fromDate: string, toDate:string;
+    let loading = false;
+
+    async function createTurns() {
+        loading = true;
+        let from = new Date(fromDate);
+        let to = new Date(toDate);
+
+        if (from > to) {
+            fromDate = '';
+            toDate = '';
+
+            $toastMessageAlert = `From Date cannot be bigger than To Date`;
+			$toastAlert = true;
+			setTimeout(() => {
+				$toastAlert = false;
+			}, 8000);
+            return;
+        }
+
+        let weekday: string, userList, users, incidences, exit = false, continueLoop = false;
+
+        for (var d = from; d <= to; d.setDate(d.getDate() + 1)) {
+            weekday = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'][d.getDay()]
+            //Loop over schedules
+            $schedules.forEach(async (schedule) => {
+                if (schedule.weekday === weekday) {
+                    userList = await db.availability.where({schedule_id: schedule.id}).toArray();
+                    if (userList.length == 0) {
+                        $toastMessageWarning = `There is no publishers that can be assigned on ${d.toISOString().split('T')[0]}`;
+                        $toastWarning = true;
+                        setTimeout(() => {
+                            $toastWarning = false;
+                        }, 8000);
+                    }
+                    //Loop over availabilities
+                    userList.forEach(async (availabilities) => {
+                        users = await db.user.where({id: availabilities.user_id}).toArray();
+                        if (users.length == 0) {
+                            $toastMessageWarning = `There is no publishers that can be assigned on ${d.toISOString().split('T')[0]}`;
+                            $toastWarning = true;
+                            setTimeout(() => {
+                                $toastWarning = false;
+                            }, 8000);
+                        }
+                        //Loop over users
+                        users.forEach(async (user) => {
+                            incidences = await db.incidence.where({user_id: user.id}).toArray();
+                            //Loop over incidences
+                            incidences.forEach(async (incidence) => {
+                                if (incidence.start_date >= d.toISOString().split('T')[0] && d.toISOString().split('T')[0] <= incidence.end_date) {
+                                    continueLoop = true;
+                                    return;
+                                }
+                            });
+                        });
+                    });
+                }
+            });
+        }
+
+        //fromDate = '';
+        //toDate = '';
+        loading = false;
+    }
 </script>
 
 <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto">
@@ -16,13 +82,20 @@
         <div class="mt-1 mb-4 flex flex-row justify-around">
             <Label>
                 From:
-                <Input type="date" />
+                <Input type="date" bind:value={fromDate}/>
             </Label>
             <Label>
                 To:
-                <Input type="date" />
+                <Input type="date" bind:value={toDate}/>
             </Label>
-		    <Button color="blue" class="w-8/12">Create Turns</Button>
+		    <Button color="blue" class="w-8/12" on:click={createTurns}>
+                {#if loading}
+                    <Spinner class="me-3" size="4" color="white" />
+                    Creating...
+                {:else}
+                    Create Turns
+                {/if}
+            </Button>
         </div>
 	</Card>
     {#if $turns}
