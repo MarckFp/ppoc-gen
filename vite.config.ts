cd5f2b846230937a0ad30f8a996fe67d449509d8
Marcos Fernández Pérez <mfernandez@edpuzzle.com>
2024-04-11 14:38:38 +0200

    Fix again icons

--- vite.config.ts
+++ vite.config.ts
@@ -32,12 +32,12 @@ export default defineConfig({
 				theme_color: '#eb4034',
 				icons: [
 					{
-						src: '/android-chrome-192x192.png',
+						src: 'android-chrome-192x192.png',
 						sizes: '192x192',
 						type: 'image/png'
 					},
 					{
-						src: '/android-chrome-512x512.png',
+						src: 'android-chrome-512x512.png',
 						sizes: '512x512',
 						type: 'image/png'
 					},
