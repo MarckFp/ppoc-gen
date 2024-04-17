122ce52483d7983c477dfcd878d4f0e8664ef987
Marcos Fernández Pérez <mfernandez@edpuzzle.com>
2024-04-17 07:53:14 +0200

    Fixed visual issues on home screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,10 +18,10 @@
 	}
 </script>
 
-<div class="flex flex-col items-center">
+<div class="flex flex-col items-center m-5">
 	{#if $congregation}
-		<Card class="m-5 text-center" size="xl" padding="xl">
-			<h5 class="mb-2 text-3xl font-bold text-gray-900 dark:text-white">
+		<Card class="text-center" size="xl" padding="xl">
+			<h5 class="text-3xl font-bold text-gray-900 dark:text-white">
 				{$_('home.welcome')}
 				{$congregation[0].name}
 			</h5>
