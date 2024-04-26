f581dc37df96a9772c957ba6683ca0f49eaa3576
Marcos Fernández Pérez <mfernandez@edpuzzle.com>
2024-04-26 13:35:33 +0200

    Fixed GH Action

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test'
 
 test('Congregation creation', async ({ page }) => {
   await page.goto('/')
@@ -6,8 +6,9 @@ test('Congregation creation', async ({ page }) => {
   await page.getByTestId('create-lang').selectOption('en')
   await page.getByTestId('create-weekday').selectOption('monday')
   await page.getByTestId('create-btn').click()
+  await page.waitForTimeout(1000)
   expect(page.getByTestId('toast')).toContainText('Congregation Added successfully')
-});
+})
 
 test('Navbar menu', async ({ page }) => {
   await page.goto('/')
@@ -16,18 +17,18 @@ test('Navbar menu', async ({ page }) => {
   await page.getByTestId('create-weekday').selectOption('monday')
   await page.getByTestId('create-btn').click()
   await page.getByTestId('navbar-publishers').click()
-  await page.waitForTimeout(1000);
+  await page.waitForTimeout(1000)
   expect(page.getByTestId('publishers-create-btn')).toContainText('Create new Publisher')
   await page.getByTestId('navbar-schedules').click()
-  await page.waitForTimeout(1000);
+  await page.waitForTimeout(1000)
   expect(page.getByTestId('schedules-create-btn')).toContainText('Create new Schedule')
   await page.getByTestId('navbar-turns').click()
-  await page.waitForTimeout(1000);
+  await page.waitForTimeout(1000)
   expect(page.getByTestId('turns-generate-btn')).toContainText('Generate Turns')
   await page.getByTestId('navbar-incidences').click()
-  await page.waitForTimeout(1000);
+  await page.waitForTimeout(1000)
   expect(page.getByTestId('incidences-create-btn')).toContainText('Create new Incidence')
   await page.getByTestId('navbar-settings').click()
-  await page.waitForTimeout(1000);
+  await page.waitForTimeout(1000)
   expect(page.getByTestId('settings-update-btn')).toContainText('Update Congregation')
-});
+})
