596916b306de916bc50ce3971c0519691f981585
Marcos Fernández Pérez <mfernandez@edpuzzle.com>
2024-05-17 12:59:26 +0200

    Added a bottom navbar test

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,5 +1,5 @@
-<script>
-	import {Navbar, NavBrand, NavLi, NavUl, NavHamburger, DarkMode} from 'flowbite-svelte'
+<script lang="ts">
+	import {Navbar, NavBrand, NavLi, NavUl, NavHamburger, DarkMode, BottomNav, BottomNavItem} from 'flowbite-svelte'
 	import {base} from '$app/paths'
 	import {
 		UsersGroupOutline,
@@ -9,49 +9,94 @@
 		EditOutline
 	} from 'flowbite-svelte-icons'
 	import {_} from 'svelte-i18n'
+	import {page} from '$app/stores'
+
+	$: activeUrl = $page.url.pathname
+	let mobile: boolean = false
+
+	//Media Queries for Calendar View
+	const mediaQuery = window.matchMedia('(width <= 640px)')
+	mediaQuery.addEventListener('change', ({matches}) => {
+		if (matches) {
+			mobile = true
+		} else {
+			mobile = false
+		}
+	})
+	if (mediaQuery.matches) {
+		mobile = true
+	} else {
+		mobile = false
+	}
 </script>
 
-<Navbar let:NavContainer class="dark:bg-gray-700 print:hidden">
-	<NavContainer class="rounded-lg border bg-white px-5 py-2 dark:bg-gray-600">
-		<NavBrand href="{base}/">
+{#if mobile}
+	<BottomNav {activeUrl} position="fixed" classInner="grid-cols-7">
+		<BottomNavItem href="{base}/">
 			<img src="favicon.ico" class="me-3 h-6 rounded-lg sm:h-9" alt="PPOC Gen Logo" />
-			<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">PPOC Gen</span>
-		</NavBrand>
-		<div class="flex md:order-2">
+		</BottomNavItem>
+		<BottomNavItem href="{base}/publishers">
+			<UsersGroupOutline size="lg" color="red" class="dark:text-primary-400" />
+		</BottomNavItem>
+		<BottomNavItem href="{base}/schedules">
+			<CalendarMonthOutline size="lg" color="red" class="dark:text-primary-400" />
+		</BottomNavItem>
+		<BottomNavItem href="{base}/turns">
+			<EditOutline size="lg" color="red" class="dark:text-primary-400" />
+		</BottomNavItem>
+		<BottomNavItem href="{base}/incidences">
+			<BellRingOutline size="lg" color="red" class="dark:text-primary-400" />
+		</BottomNavItem>
+		<BottomNavItem href="{base}/settings">
+			<CogOutline size="lg" color="red" class="dark:text-primary-400" />
+		</BottomNavItem>
+		<BottomNavItem>
 			<DarkMode class="black ml-2 dark:border-gray-800 dark:text-primary-200" />
-			<NavHamburger data-testid="navbar-hamburger" />
-		</div>
-		<NavUl class="order-1">
-			<NavLi href="{base}/publishers" data-testid="navbar-publishers">
-				<div class="flex flex-row dark:text-white">
-					{$_('navbar.publishers')}
-					<UsersGroupOutline size="lg" color="red" class="dark:text-primary-400" />
-				</div>
-			</NavLi>
-			<NavLi href="{base}/schedules" data-testid="navbar-schedules">
-				<div class="flex flex-row dark:text-white">
-					{$_('navbar.schedules')}
-					<CalendarMonthOutline size="lg" color="red" class="dark:text-primary-400" />
-				</div></NavLi
-			>
-			<NavLi href="{base}/turns" data-testid="navbar-turns">
-				<div class="flex flex-row dark:text-white">
-					{$_('navbar.turns')}
-					<EditOutline size="lg" color="red" class="dark:text-primary-400" />
-				</div></NavLi
-			>
-			<NavLi href="{base}/incidences" data-testid="navbar-incidences">
-				<div class="flex flex-row dark:text-white">
-					{$_('navbar.incidences')}
-					<BellRingOutline size="lg" color="red" class="dark:text-primary-400" />
-				</div></NavLi
-			>
-			<NavLi href="{base}/settings" data-testid="navbar-settings">
-				<div class="flex flex-row dark:text-white">
-					{$_('navbar.settings')}
-					<CogOutline size="lg" color="red" class="dark:text-primary-400" />
-				</div></NavLi
-			>
-		</NavUl>
-	</NavContainer>
-</Navbar>
+		</BottomNavItem>
+	</BottomNav>
+{:else}
+	<Navbar let:NavContainer class="dark:bg-gray-700 print:hidden">
+		<NavContainer class="rounded-lg border bg-white px-5 py-2 dark:bg-gray-600">
+			<NavBrand href="{base}/">
+				<img src="favicon.ico" class="me-3 h-6 rounded-lg sm:h-9" alt="PPOC Gen Logo" />
+				<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">PPOC Gen</span>
+			</NavBrand>
+			<div class="flex md:order-2">
+				<DarkMode class="black ml-2 dark:border-gray-800 dark:text-primary-200" />
+				<NavHamburger data-testid="navbar-hamburger" />
+			</div>
+			<NavUl class="order-1">
+				<NavLi href="{base}/publishers" data-testid="navbar-publishers">
+					<div class="flex flex-row dark:text-white">
+						{$_('navbar.publishers')}
+						<UsersGroupOutline size="lg" color="red" class="dark:text-primary-400" />
+					</div>
+				</NavLi>
+				<NavLi href="{base}/schedules" data-testid="navbar-schedules">
+					<div class="flex flex-row dark:text-white">
+						{$_('navbar.schedules')}
+						<CalendarMonthOutline size="lg" color="red" class="dark:text-primary-400" />
+					</div></NavLi
+				>
+				<NavLi href="{base}/turns" data-testid="navbar-turns">
+					<div class="flex flex-row dark:text-white">
+						{$_('navbar.turns')}
+						<EditOutline size="lg" color="red" class="dark:text-primary-400" />
+					</div></NavLi
+				>
+				<NavLi href="{base}/incidences" data-testid="navbar-incidences">
+					<div class="flex flex-row dark:text-white">
+						{$_('navbar.incidences')}
+						<BellRingOutline size="lg" color="red" class="dark:text-primary-400" />
+					</div></NavLi
+				>
+				<NavLi href="{base}/settings" data-testid="navbar-settings">
+					<div class="flex flex-row dark:text-white">
+						{$_('navbar.settings')}
+						<CogOutline size="lg" color="red" class="dark:text-primary-400" />
+					</div></NavLi
+				>
+			</NavUl>
+		</NavContainer>
+	</Navbar>
+{/if}
