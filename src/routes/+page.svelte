d7f0279f8e3f019e0198633d581d9926dc052ee4
Marcos Fernández Pérez <mfernandez@edpuzzle.com>
2024-05-27 09:47:22 +0200

    Removed Windows ARM since it produces the same files, also added direct download buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,24 @@
 <script lang="ts">
 	import {liveQuery} from 'dexie'
 	import {db} from '$lib/db'
-	import {Card, GradientButton, Toast} from 'flowbite-svelte'
-	import {GithubSolid, BookSolid, ExclamationCircleSolid} from 'flowbite-svelte-icons'
+	import {Card, Dropdown, DropdownDivider, DropdownItem, GradientButton, Helper, Toast} from 'flowbite-svelte'
+	import {
+		GithubSolid,
+		BookSolid,
+		ExclamationCircleSolid,
+		ChevronDownOutline,
+		WindowsSolid,
+		AppleSolid,
+		DesktopPcSolid
+	} from 'flowbite-svelte-icons'
 	import {_} from 'svelte-i18n'
 	import Calendar from '$lib/components/Calendar.svelte'
 
 	let congregation = liveQuery(() => db.congregation.orderBy('id').first())
 	const announcement = ``
+
+	// eslint-disable-next-line
+	const version = PKG.version ?? 'unknown'
 </script>
 
 <section class="m-5 flex flex-col items-center">
@@ -22,18 +33,44 @@
 	<Card size="xl" padding="xl" class="mt-5">
 		<h2 class="mb-2 text-2xl font-bold text-gray-900 dark:text-white">{$_('home.thank-you')}</h2>
 		<hr />
-		<p class="mt-2 text-gray-900 dark:text-white">
-			{$_('home.apps')}<GradientButton href="https://github.com/MarckFp/ppoc-gen/releases" color="cyanToBlue"
-				>GitHub <GithubSolid /></GradientButton
+		<div class="mt-2 grid text-gray-900 dark:text-white {version == 'unknown' ? '' : 'md:grid-cols-2'} gap-2">
+			<GradientButton color="teal" href="https://github.com/MarckFp/ppoc-gen/blob/main/docs/README.md"
+				>{$_('home.docs')} <BookSolid class="ms-1" /></GradientButton
 			>
-		</p>
-		<p class="mt-5 text-gray-900 dark:text-white">
-			{$_('home.read-the-docs')}<GradientButton
-				color="teal"
-				href="https://github.com/MarckFp/ppoc-gen/blob/main/docs/README.md"
-				>{$_('home.docs')} <BookSolid /></GradientButton
-			>
-		</p>
+			{#if version != 'unknown'}
+				<GradientButton color="cyanToBlue">
+					{$_('home.download-btn')}
+					<GithubSolid class="ms-1" />
+					<ChevronDownOutline class="ms-2 h-6 w-6 text-white dark:text-white" />
+				</GradientButton>
+				<Dropdown>
+					<DropdownItem
+						href="https://github.com/MarckFp/ppoc-gen/releases/download/{version}/PPOC.Gen_{version}_x64-setup.exe"
+					>
+						<div class="flex flex-row gap-1"><span>Windows</span><WindowsSolid /></div>
+					</DropdownItem>
+					<DropdownDivider />
+					<DropdownItem
+						href="https://github.com/MarckFp/ppoc-gen/releases/download/{version}/PPOC.Gen_{version}_x64.dmg"
+					>
+						<div class="flex flex-row gap-1"><span>MacOS</span><AppleSolid /></div>
+						<Helper>Intel</Helper>
+					</DropdownItem>
+					<DropdownItem
+						href="https://github.com/MarckFp/ppoc-gen/releases/download/{version}/PPOC.Gen_{version}_aarch64.dmg"
+					>
+						<div class="flex flex-row gap-1"><span>MacOS</span><AppleSolid /></div>
+						<Helper>arm</Helper>
+					</DropdownItem>
+					<DropdownDivider />
+					<DropdownItem
+						href="https://github.com/MarckFp/ppoc-gen/releases/download/{version}/ppoc-gen_{version}_amd64.deb"
+					>
+						<div class="flex flex-row gap-1"><span>Linux</span><DesktopPcSolid /></div>
+					</DropdownItem>
+				</Dropdown>
+			{/if}
+		</div>
 	</Card>
 	{#if announcement != ''}
 		<Toast color="orange" class="my-2 w-full max-w-screen-xl text-gray-900 dark:text-white">
