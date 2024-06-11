f8b8a5466e6779c3bcc4c1fe4c99cde6918a263f
Marcos Fernández Pérez <mfernandez@edpuzzle.com>
2024-06-11 16:02:40 +0200

    Added german support too

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -4,10 +4,12 @@ import en from '../i18n/en.json'
 import es from '../i18n/es.json'
 import fr from '../i18n/fr.json'
 import pt from '../i18n/pt.json'
+import de from '../i18n/de.json'
 addMessages('en', en)
 addMessages('es', es)
 addMessages('fr', fr)
 addMessages('pt', pt)
+addMessages('de', de)
 init({
 	fallbackLocale: 'en',
 	initialLocale: getLocaleFromNavigator()
