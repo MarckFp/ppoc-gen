d1e802338f25425f0c07c050203fbe399294bc34
Marcos Fernández Pérez <mfernandez@edpuzzle.com>
2024-06-11 15:28:09 +0200

    Added French Support thanks to chat gpt

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,8 +2,10 @@ import {addMessages, init, getLocaleFromNavigator} from 'svelte-i18n'
 
 import en from '../i18n/en.json'
 import es from '../i18n/es.json'
+import fr from '../i18n/fr.json'
 addMessages('en', en)
 addMessages('es', es)
+addMessages('fr', fr)
 init({
 	fallbackLocale: 'en',
 	initialLocale: getLocaleFromNavigator()
