29e5b4b54937271655130b9d255f1cf243c215c1
Marcos Fernández Pérez <mfernandez@edpuzzle.com>
2024-06-12 13:36:55 +0200

    Added Russian support

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -10,6 +10,8 @@ import ro from '../i18n/ro.json'
 import zh from '../i18n/zh.json'
 import jp from '../i18n/jp.json'
 import ko from '../i18n/ko.json'
+import ru from '../i18n/ru.json'
+//import ar from '../i18n/ar.json'
 
 addMessages('en', en)
 addMessages('es', es)
@@ -21,6 +23,8 @@ addMessages('ro', ro)
 addMessages('zh', zh)
 addMessages('jp', jp)
 addMessages('ko', ko)
+addMessages('ru', ru)
+//addMessages('ar', ar) //TODO: We need to support to serve the page on a different word order since it's red from left to right instead of right to left
 
 init({
 	fallbackLocale: 'en',
