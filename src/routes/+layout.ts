3ef0cc29abc1e755f03b8988306527be027e3396
Marcos Fernández Pérez <mfernandez@edpuzzle.com>
2024-06-12 12:58:38 +0200

    Added support to Korean

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -5,20 +5,22 @@ import es from '../i18n/es.json'
 import fr from '../i18n/fr.json'
 import pt from '../i18n/pt.json'
 import de from '../i18n/de.json'
-import zh from '../i18n/zh.json'
 import hi from '../i18n/hi.json'
 import ro from '../i18n/ro.json'
+import zh from '../i18n/zh.json'
 import jp from '../i18n/jp.json'
+import ko from '../i18n/ko.json'
 
 addMessages('en', en)
 addMessages('es', es)
 addMessages('fr', fr)
 addMessages('pt', pt)
 addMessages('de', de)
-addMessages('zh', zh)
 addMessages('hi', hi)
 addMessages('ro', ro)
+addMessages('zh', zh)
 addMessages('jp', jp)
+addMessages('ko', ko)
 
 init({
 	fallbackLocale: 'en',
