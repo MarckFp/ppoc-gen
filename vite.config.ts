5e085bc1464cb10c6ee7e9ab679ee79ef2e0c608
Marcos Fernández Pérez <mfernandez@edpuzzle.com>
2024-06-14 09:35:36 +0200

    Removed spotlight

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite'
 import { SvelteKitPWA } from '@vite-pwa/sveltekit'
 import { readFileSync } from 'fs'
 import { fileURLToPath } from 'url'
-import spotlightSidecar from '@spotlightjs/sidecar/vite-plugin'
 
 const file = fileURLToPath(new URL('package.json', import.meta.url))
 const json = readFileSync(file, 'utf8')
@@ -39,7 +38,6 @@ export default defineConfig({
 		minify: 'esbuild',
 	},
 	plugins: [
-        spotlightSidecar(),
         sentrySvelteKit({
             adapter: 'other',
             sourceMapsUploadOptions: {
